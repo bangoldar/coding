ee74a8811d0a0f1e4244d7431d6912b8f776a626
bangoldar <arkbangoldar@gmail.com>
2026-03-08 20:09:27 +0000

    .

--- lessons/lesson6.py
+++ lessons/lesson6.py
@@ -1,12 +1,29 @@
 from typing import List
 
 
+'''     n1  n2
+         |   |
+   i = 0 1 2 3
+nums = 8 2 9 7
 
+loop 0 1 2: # 2
+    loop 1 2 3:
+        sum = 2 + 3
+
+'''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return 1
-        ...
-        
+        for i in range(0,len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j 
 
-res = Solution().twoSum(nums=[2,7,13,15],target=9)
+res = Solution().twoSum(nums=[2,13,15,7],target=9)
 print(res)
+
+#     01234
+# ex = 'hello'
+# for i in range(len(ex)): # 5 0-4
+#     e = ex[i]
+#     print(e,i)
+#     ...
---
+++ lessons/lesson7.py
@@ -0,0 +1,36 @@
+
+### dict ###
+
+# d1 = {'name':'Alex'}
+# name = d1['name']
+
+# d1['surname'] = 'Petrov'
+# surname = d1['surname']
+# print(name,surname)
+
+# d1['surname'] = 'Moroz'
+# surname = d1['surname']
+
+# print(name,surname)
+
+
+# for k in d1:
+#     print(k,d1[k])
+
+# for k in d1.keys():
+#     print(k,d1[k])
+
+# for v in d1.values():
+#     print(v)
+
+# for k,v in d1.items():
+#     print(k,v)
+
+'''
+text = 'hello'
+res = {'h':1,'e':1,'l':2,'o':1}
+'''
+word = input('enter word >>')
+d1 = {}
+for letter in word:
+    for 
